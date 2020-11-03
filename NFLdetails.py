c39cbeb9030ac69d19a21e39fe8bc9b2b1bc0c29
Degentleman <42943580+Degentleman@users.noreply.github.com>
2020-11-03 15:16:37 -0600

    Updated NFLdetails file
    Added context to lines of code that may not be understandable for people new to Python.

--- NFLdetails.py
+++ NFLdetails.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Nov  2 11:39:59 2020
+Created on Mon Apr 20 04:20:00 2020
 
 @author: Degentleman
 """
 import pandas as pd
 import numpy as np
-for year in range(1999,2021):
+for year in range(2020,2021):
     SCORES = pd.DataFrame()
     print('____')
     print(year)
@@ -16,6 +16,7 @@ for year in range(1999,2021):
                            header=0, na_filter=False,
                            error_bad_lines=False,
                            low_memory=False)
+    # Add the columns you would like to include from the original play-by-play file in the details list below.
     details = ['season', 'game_id', 'week',
         'home_team', 'away_team', 'div_game', 'qtr', 'drive',
         'game_seconds_remaining', 'yardline_100',
@@ -24,9 +25,11 @@ for year in range(1999,2021):
         'total_home_score', 'total_away_score', 'ha_differential', 'curr_total',
         'home_score', 'away_score', 'result', 'total',
     'spread_line','total_line', 'desc']
+    # Fill NA values in scoring with zeros, which assumes NA value is because no scoring has occured.
     columns = ['posteam_score', 'defteam_score', 'score_differential']
     DB[columns] = DB[columns].replace('NA', np.nan).fillna(0)
     gameIDs = np.unique(DB.game_id)
+    #For each game where the team with the ball is known, take the first play of the drive and evaluate current score.
     for game in gameIDs:
         print(game)
         gameDF = DB[(DB.game_id == game) & (DB.desc != 'END GAME') & (~DB.posteam.isin(['NA','']))]
@@ -39,4 +42,5 @@ for year in range(1999,2021):
             # Using list selection to return first instance of duplicate play id
             SCORES = pd.concat([SCORES, row.iloc[[0]][details]])
     print('Results from '+str(year)+' are done!')
+    # Save CSV of details to current working directory.
     SCORES.to_csv('NFL '+str(year)+' Quarter and Drive Scoring DF.csv', index=False)
