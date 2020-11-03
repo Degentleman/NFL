64f6ddb20f4269a59b9de1a65feedc6bcc9ab6e3
Degentleman <42943580+Degentleman@users.noreply.github.com>
2020-11-03 17:11:10 -0600

    Updated NFLpbp
    Changed script to identify any passer as "QB" to account for plays where QB doesn't throw the pass.

--- NFLpbp.py
+++ NFLpbp.py
@@ -16,13 +16,13 @@ DB = pd.read_csv('pbp/play_by_play_{year}.csv.gz'.format(year=year),
                        error_bad_lines=False,
                        low_memory=False)
 #Specify columns to fillna values, this example is mainly QB-related columns
-columns = ['posteam','defteam',
+columns = ['posteam','defteam', 'drive',
            'yards_gained', 'air_yards',
            'yards_after_catch', 'score_differential',
            'ep', 'epa', 'air_epa', 'yac_epa',
-           'air_wpa', 'yac_wpa',
+           'air_wpa', 'yac_wpa', 
            'pass_attempt', 'pass_touchdown', 'complete_pass',
-           'passer_player_name',
+           'passer_player_name', 'passer', 'pass',
            'cp', 'xyac_mean_yardage']
 
 DB[columns] = DB[columns].replace('NA', np.nan).fillna('')
@@ -37,16 +37,17 @@ for Team in Teams:
 
     print(Team, year)
 
-    qb_list = list(np.unique(teamDF[(teamDF.posteam == Team)].passer_player_name))
-    if '' in qb_list:
-        qb_list.remove('')
-    for qb in qb_list:
+    passer_list = list(np.unique(teamDF[(teamDF.posteam == Team)].passer))
+    if '' in passer_list:
+        passer_list.remove('')
+    for qb in passer_list:
         print(qb)
-        qbDF = teamDF[(teamDF.passer_player_name == qb) & (teamDF.pass_attempt == '1') & (teamDF.cp != '') & (teamDF.xyac_mean_yardage != '')].infer_objects()
+        qbDF = teamDF[(teamDF.passer == qb) & (teamDF['pass'] == 1) & (teamDF.xyac_mean_yardage != '')].infer_objects()
         gameIDs = np.unique(qbDF.game_id)
         for game in gameIDs:
             print(game)
             gameDF = qbDF[(qbDF.game_id == game) & (qbDF.desc != 'END GAME') & (~qbDF.posteam.isin(['NA','']))]
+            Opp = np.unique(gameDF.defteam)
             drives = np.sort(np.array(np.unique(gameDF.drive),dtype=int))
             for drive in drives:
                 sampleDF = gameDF[(gameDF.drive == str(drive))]
@@ -55,8 +56,8 @@ for Team in Teams:
                 cp = np.array(sampleDF[['cp']], dtype=float)
                 xyac_loc = np.array(sampleDF[['xyac_mean_yardage']], dtype=float)
                 cpXyac = cp*xyac_loc
-                entry = [year, Team, game, drive, qb, attempts, completions, round(np.sum(cp),3), round(np.sum(xyac_loc),3)
+                entry = [year, Team, Opp, game, drive, qb, attempts, completions, round(np.sum(cp),3), round(np.sum(xyac_loc),3)
                  , round((np.sum(xyac_loc)/np.sum(cp)),3), round(np.sum(cpXyac),3)]
                 data.append(entry)
     print('___')
-qb_perf = pd.DataFrame(data, columns=['Season', 'Team', 'gameID', 'Drive', 'QB', 'Att', 'Comps', 'CP_Sum', 'Avg_XYAC_Sum', 'XYACperCP', 'CP*XYAC_Sum'])
+qb_perf = pd.DataFrame(data, columns=['Season', 'Team', 'Opp', 'gameID', 'Drive', 'QB', 'Att', 'Comps', 'CP_Sum', 'Avg_XYAC_Sum', 'XYACperCP', 'CP*XYAC_Sum'])
