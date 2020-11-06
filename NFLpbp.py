7c741d7a66d6353af18c1603f13b6df602397d0c
Degentleman <42943580+Degentleman@users.noreply.github.com>
2020-11-06 12:37:01 -0600

    Updated NFLpbp.py
    Updated the NFLpbp.py file to include a detailed list of columns so users can manually specify which columns they don't want to be included in their output.

--- NFLpbp.py
+++ NFLpbp.py
@@ -7,57 +7,116 @@ Created on Mon Apr 20 04:20:00 2020
 """
 import pandas as pd
 import numpy as np
-# Specify the year of the play-by-play data you want to load. Must be in working directory.
-year = 2020
-#Create pandas DataFrame from nflfastR data
-DB = pd.read_csv('pbp/play_by_play_{year}.csv.gz'.format(year=year), 
-                       sep=",", compression='gzip', 
-                       header=0, na_filter=False,
-                       error_bad_lines=False,
-                       low_memory=False)
-#Specify columns to fillna values, this example is mainly QB-related columns
-columns = ['posteam','defteam', 'drive',
-           'yards_gained', 'air_yards',
-           'yards_after_catch', 'score_differential',
-           'ep', 'epa', 'air_epa', 'yac_epa',
-           'air_wpa', 'yac_wpa', 
-           'pass_attempt', 'pass_touchdown', 'complete_pass',
-           'passer_player_name', 'passer', 'pass',
-           'cp', 'xyac_mean_yardage']
-
-DB[columns] = DB[columns].replace('NA', np.nan).fillna('')
-# Create a list of all teams in DB object.
-Teams = list(np.unique(DB.posteam))
-Teams.remove('')
-#Create blank list as data object to append as the script parses through the file.
-data = []
-# Look at each team in the DB and their unique QB data per drive.
-for Team in Teams:
-    teamDF = DB[((DB.posteam == Team) | (DB.defteam == Team))]
-
-    print(Team, year)
-
-    passer_list = list(np.unique(teamDF[(teamDF.posteam == Team)].passer))
-    if '' in passer_list:
-        passer_list.remove('')
-    for qb in passer_list:
-        print(qb)
-        qbDF = teamDF[(teamDF.passer == qb) & (teamDF['pass'] == 1) & (teamDF.xyac_mean_yardage != '')].infer_objects()
-        gameIDs = np.unique(qbDF.game_id)
-        for game in gameIDs:
-            print(game)
-            gameDF = qbDF[(qbDF.game_id == game) & (qbDF.desc != 'END GAME') & (~qbDF.posteam.isin(['NA','']))]
-            Opp = np.unique(gameDF.defteam)
-            drives = np.sort(np.array(np.unique(gameDF.drive),dtype=int))
-            for drive in drives:
-                sampleDF = gameDF[(gameDF.drive == str(drive))]
-                attempts = len(sampleDF)
-                completions = len(sampleDF[(sampleDF.complete_pass == '1')])
-                cp = np.array(sampleDF[['cp']], dtype=float)
-                xyac_loc = np.array(sampleDF[['xyac_mean_yardage']], dtype=float)
-                cpXyac = cp*xyac_loc
-                entry = [year, Team, Opp, game, drive, qb, attempts, completions, round(np.sum(cp),3), round(np.sum(xyac_loc),3)
-                 , round((np.sum(xyac_loc)/np.sum(cp)),3), round(np.sum(cpXyac),3)]
-                data.append(entry)
-    print('___')
-qb_perf = pd.DataFrame(data, columns=['Season', 'Team', 'Opp', 'gameID', 'Drive', 'QB', 'Att', 'Comps', 'CP_Sum', 'Avg_XYAC_Sum', 'XYACperCP', 'CP*XYAC_Sum'])
+# Create pandas DataFrame which will be used to append data to.
+sampleDB = pd.DataFrame()
+# Specify the range of dates to use in the sample, the files must be in your working directory.
+for year in range(2020,2021):
+    DB = pd.read_csv('pbp/play_by_play_{year}.csv.gz'.format(year=year), 
+                           sep=",", compression='gzip', 
+                           header=0, na_filter=False,
+                           error_bad_lines=False,
+                           low_memory=False)
+    # Below is a list of columns to pass if you want the NA values changed to blanks.
+    na_cols_to_check = ['posteam','defteam', 'drive',
+               'yards_gained', 'air_yards',
+               'yards_after_catch', 'score_differential',
+               'ep', 'epa', 'air_epa', 'yac_epa',
+               'air_wpa', 'yac_wpa', 
+               'pass_attempt', 'pass_touchdown', 'complete_pass',
+               'passer_player_name', 'passer', 'pass',
+               'cp', 'xyac_mean_yardage']
+    DB[na_cols_to_check] = DB[na_cols_to_check].replace('NA', np.nan).fillna('')
+    
+    # All teams will be included in the sample unless you pass a list of specific teams below.
+    Teams = list(np.unique(DB.posteam))
+    Teams.remove('')
+    for Team in Teams:
+        print(Team, year)
+        teamDF = DB[((DB.posteam == Team) | (DB.defteam == Team))]
+        passer_list = list(np.unique(teamDF[(teamDF.posteam == Team)].passer))
+        if '' in passer_list:
+            passer_list.remove('')
+        for qb in passer_list:
+            print(qb)
+            qbDF = teamDF[(teamDF.passer == qb) & (teamDF['pass'] == 1) & (teamDF.xyac_mean_yardage != '')].infer_objects()
+            gameIDs = np.unique(qbDF.game_id)
+            for game in gameIDs:
+                print(game)
+                gameDF = qbDF[(qbDF.game_id == game) & (qbDF.desc != 'END GAME') & (~qbDF.posteam.isin(['NA','']))]
+                Opp = np.unique(gameDF.defteam)
+                drives = np.sort(np.array(np.unique(gameDF.drive),dtype=int))
+                for drive in drives:
+                    sampleDF = gameDF[(gameDF.drive == str(drive))]
+                    sampleDB = pd.concat([sampleDB, sampleDF])
+        print('___')
+# Below is a list of columns to use for output and can be changed manually to increase readability and decrease file size.
+columns_to_use = ['season', 'week', 'game_id', 'game_date', 'start_time',
+       'time_of_day', 'stadium_id', 'home_team', 'away_team', 'div_game',
+       'play_id', 'time', 'end_clock_time', 'play_clock', 'posteam',
+       'posteam_type', 'defteam', 'game_half', 'qtr', 'down', 'ydstogo',
+       'yardline_100', 'goal_to_go', 'desc', 'drive',
+       'drive_game_clock_start', 'drive_game_clock_end', 'series',
+       'series_success', 'series_result', 'quarter_seconds_remaining',
+       'half_seconds_remaining', 'game_seconds_remaining', 'pass_length',
+       'pass_location', 'qb_hit', 'sack', 'complete_pass',
+       'incomplete_pass', 'air_yards', 'yards_after_catch', 'yards_gained',
+       'cp', 'cpoe', 'ep', 'epa', 'air_epa', 
+       'yac_epa', 'comp_air_epa', 'comp_yac_epa',
+       'air_wpa', 'yac_wpa', 'comp_air_wpa', 'comp_yac_wpa',
+       'wp', 'wpa', 'def_wp', 'vegas_wp', 'home_wp', 'away_wp',
+       'home_timeouts_remaining', 'away_timeouts_remaining',
+       'total_home_score', 'total_away_score', 'posteam_score',
+       'defteam_score', 'score_differential', 
+       'shotgun', 'no_huddle', 'qb_dropback', 'qb_kneel', 'qb_spike', 'qb_scramble', 
+       'first_down', 'third_down_converted', 'third_down_failed', 
+       'fourth_down_converted', 'fourth_down_failed', 
+       'no_score_prob', 'fg_prob', 'safety_prob', 'td_prob', 
+       'opp_fg_prob', 'opp_safety_prob', 'opp_td_prob',
+       'home_wp_post', 'away_wp_post', 'vegas_home_wp',
+       'total_home_pass_epa', 'total_away_pass_epa',
+       'total_home_comp_air_epa', 'total_away_comp_air_epa', 
+       'total_home_comp_yac_epa', 'total_away_comp_yac_epa', 
+       'total_home_raw_air_epa', 'total_away_raw_air_epa', 
+       'total_home_raw_yac_epa', 'total_away_raw_yac_epa', 
+       'total_home_pass_wpa', 'total_away_pass_wpa', 
+       'total_home_comp_air_wpa', 'total_away_comp_air_wpa', 
+       'total_home_comp_yac_wpa', 'total_away_comp_yac_wpa', 
+       'total_home_raw_air_wpa', 'total_away_raw_air_wpa', 
+       'total_home_raw_yac_wpa', 'total_away_raw_yac_wpa', 
+       'interception', 'safety', 'penalty',
+       'penalty_type', 'penalty_yards',
+       'penalty_team', 'penalty_player_name',
+       'touchdown', 'pass_touchdown', 'two_point_attempt', 
+       'tackled_for_loss', 'solo_tackle',
+       'assist_tackle', 'lateral_reception', 
+       'passer_player_name', 'receiver_player_name', 'interception_player_name',
+       'tackle_for_loss_1_player_name', 'tackle_for_loss_2_player_name',
+       'qb_hit_1_player_name', 'qb_hit_2_player_name',
+       'fumble', 'fumble_forced',
+       'fumble_not_forced', 'fumble_out_of_bounds', 'fumble_lost',
+       'forced_fumble_player_1_team', 'forced_fumble_player_2_team',
+       'solo_tackle_1_team', 'solo_tackle_2_team',
+       'assist_tackle_1_team', 'assist_tackle_2_team', 
+       'assist_tackle_3_team', 'assist_tackle_4_team', 
+       'pass_defense_1_player_name', 'pass_defense_2_player_name', 
+       'fumbled_1_team', 'fumbled_1_player_name', 
+       'fumbled_2_team', 'fumbled_2_player_name',
+       'fumble_recovery_1_team', 'fumble_recovery_1_yards',
+       'fumble_recovery_1_player_name', 'fumble_recovery_2_team',
+       'fumble_recovery_2_yards', 'fumble_recovery_2_player_name',
+       'replay_or_challenge', 'replay_or_challenge_result', 
+       'end_yard_line', 'fixed_drive', 'fixed_drive_result',
+       'drive_play_count', 'drive_time_of_possession',
+       'drive_first_downs', 'drive_inside20', 'drive_ended_with_score',
+       'drive_quarter_start', 'drive_quarter_end',
+       'drive_yards_penalized', 'drive_start_transition',
+       'drive_end_transition', 'drive_start_yard_line',
+       'drive_end_yard_line', 'drive_play_id_started',
+       'drive_play_id_ended', 'away_score', 'home_score', 'location',
+       'result', 'total', 'spread_line', 'total_line', 'passer',
+       'passer_jersey_number', 'receiver', 'receiver_jersey_number',
+       'qb_epa', 'xyac_epa', 'xyac_mean_yardage', 'xyac_median_yardage',
+       'xyac_success', 'xyac_fd', 'home_coach', 'away_coach',
+       'game_stadium', 'roof', 'weather']
+# Create a new DataFrame using specified columns above.
+newDF = sampleDB[columns_to_use]
